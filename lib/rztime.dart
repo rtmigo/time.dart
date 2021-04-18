b2920b0279ca8128b2177f07491f25a610834f11
rtmigo <art.galkin@gmail.com>
2021-04-18 15:42:04 +0300

    initial commit

---
+++ lib/rztime.dart
@@ -0,0 +1,85 @@
+import 'dart:math';
+
+DateTime? maxDateTime(Iterable<DateTime?> values) {
+  DateTime? result;
+
+  for (var t in values) {
+    if (result == null || (t != null && t.isAfter(result))) {
+      result = t;
+    }
+  }
+
+  return result;
+}
+
+DateTime nSecondsLater(double seconds) {
+  final dura = Duration(microseconds: (seconds * Duration.microsecondsPerSecond).round());
+  return DateTime.now().toUtc().add(dura);
+}
+
+Future<void> pause(num seconds) async {
+  await Future.delayed(Duration(microseconds: (seconds * Duration.microsecondsPerSecond).round()));
+}
+
+Future<void> pauseMs(num milliseconds) async {
+  await Future.delayed(
+      Duration(microseconds: (milliseconds * Duration.microsecondsPerMillisecond).round()));
+}
+
+Duration durationFromSeconds(double sec) =>
+    Duration(microseconds: (sec * Duration.microsecondsPerSecond).round());
+
+Duration durationFromMillis(double ms) =>
+    Duration(microseconds: (ms * Duration.microsecondsPerMillisecond).round());
+
+DateTime randomTimeBetween(DateTime a, DateTime b) {
+  if (b.isBefore(a) || a.isAtSameMomentAs(b)) {
+    throw ArgumentError();
+  }
+
+  int ia = a.microsecondsSinceEpoch;
+  int ib = b.microsecondsSinceEpoch;
+
+  int ic = ia + Random().nextInt(ib - ia);
+
+  final result = DateTime.fromMicrosecondsSinceEpoch(ic);
+
+  assert(result.isAfter(a) || result.isAtSameMomentAs(a));
+  assert(result.isBefore(b));
+
+  return result;
+}
+
+extension DateTimeExt on DateTime {
+
+  DateTime floorToSecond() {
+    return DateTime(this.year, this.month, this.day, this.hour, this.minute, this.second);
+  }
+
+  DateTime floorToMinute() {
+    return DateTime(this.year, this.month, this.day, this.hour, this.minute);
+  }
+
+  DateTime roundToSeconds() {
+    var result = this.floorToSecond();
+
+    final mcs = this.millisecond * Duration.microsecondsPerMillisecond + this.microsecond;
+
+    if (mcs >= (Duration.microsecondsPerSecond >> 1)) {
+      result = result.add(const Duration(seconds: 1));
+    }
+    return result;
+  }
+
+  DateTime floorToDay() {
+    return DateTime(this.year, this.month, this.day);
+  }
+
+  DateTime floorToHour() {
+    return DateTime(this.year, this.month, this.day, this.hour);
+  }
+
+  Duration durationSinceDayStart() {
+    return this.difference(this.floorToDay());
+  }
+}
---
+++ test/time_test.dart
@@ -0,0 +1,81 @@
+import 'dart:core';
+
+import 'package:rztime/rztime.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('findMax', () {
+    expect(
+        maxDateTime([
+          null,
+          DateTime(2014, 5, 1),
+          DateTime(2011, 5, 1),
+          DateTime(2019, 5, 1),
+          null,
+          DateTime(2017, 5, 1),
+        ]),
+        DateTime(2019, 5, 1));
+
+    expect(maxDateTime([null, null, null]), null);
+
+    expect(maxDateTime([]), null);
+  });
+
+  test('roundToSecond', () {
+    expect(DateTime(2021, 1, 1, 22, 36, 15, 499, 999).roundToSeconds(),
+        DateTime(2021, 1, 1, 22, 36, 15));
+    expect(DateTime(2021, 1, 1, 22, 36, 15, 500, 000).roundToSeconds(),
+        DateTime(2021, 1, 1, 22, 36, 16));
+    expect(DateTime(2021, 1, 1, 22, 36, 15, 500, 000).floorToSecond(),
+        DateTime(2021, 1, 1, 22, 36, 15));
+  });
+
+  test('sinceDayStart', () {
+    expect(DateTime(2021, 1, 1, 22, 36, 15, 499, 999).durationSinceDayStart(),
+        Duration(hours: 22, minutes: 36, seconds: 15, microseconds: 499999));
+    expect(DateTime(2021, 1, 1, 0, 5, 15, 499, 999).durationSinceDayStart().inSeconds, 315);
+  });
+
+  test('randomTimeBetween within millisecond', () {
+    final a = DateTime.now();
+    final b = DateTime.now().add(Duration(milliseconds: 1));
+
+    Set<DateTime> results = Set<DateTime>();
+
+    for (int i = 0; i <= 1000; ++i) {
+      final r = randomTimeBetween(a, b);
+      expect(r.isAfter(a) || r.isAtSameMomentAs(a), isTrue);
+      expect(r.isBefore(b), isTrue);
+      results.add(r);
+    }
+
+    expect(results.length, greaterThan(10));
+  });
+
+  test('randomTimeBetween one microsecond', () {
+    final a = DateTime.now();
+    final b = DateTime.now().add(Duration(microseconds: 1));
+
+    Set<DateTime> results = <DateTime>{};
+
+    for (int i = 0; i <= 1000; ++i) {
+      final r = randomTimeBetween(a, b);
+      expect(r.isAfter(a) || r.isAtSameMomentAs(a), isTrue);
+      expect(r.isBefore(b), isTrue);
+      results.add(r);
+    }
+
+    expect(results.length, 1); // ровно один результат, потому что интервал 1 миллисекунда
+  });
+
+  test('randomTimeBetween exotic cases', () {
+    final a = DateTime.now();
+    final b = DateTime.now().add(Duration(seconds: 1));
+
+    expect(() => randomTimeBetween(a, a), throwsArgumentError);
+    expect(() => randomTimeBetween(b, b), throwsArgumentError);
+    expect(() => randomTimeBetween(b, a), throwsArgumentError);
+
+    randomTimeBetween(a, b); // а так ок
+  });
+}
